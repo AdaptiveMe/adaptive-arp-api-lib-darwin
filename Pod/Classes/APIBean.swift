0ae1a4d2d20eff721d5ca74008b33bf3e19fd2bd
Ferran Vilà Conesa <fnva@gft.com>
2015-04-14 11:33:20 +0200

    Documentation Test

--- Pod/Classes/APIBean.swift
+++ Pod/Classes/APIBean.swift
@@ -1,63 +1,70 @@
 /**
---| ADAPTIVE RUNTIME PLATFORM |----------------------------------------------------------------------------------------
-
-(C) Copyright 2013-2015 Carlos Lozano Diez t/a Adaptive.me <http://adaptive.me>.
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
-License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 . Unless required by appli-
--cable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
-WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the  License  for the specific language governing
-permissions and limitations under the License.
-
-Original author:
-
-* Carlos Lozano Diez
-<http://github.com/carloslozano>
-<http://twitter.com/adaptivecoder>
-<mailto:carlos@adaptive.me>
-
-Contributors:
-
-* Ferran Vila Conesa
-<http://github.com/fnva>
-<http://twitter.com/ferran_vila>
-<mailto:ferran.vila.conesa@gmail.com>
-
-* See source code files for contributors.
-
-Release:
-
-* @version v2.2.0
-
--------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
+Top documentation
 */
 
 import Foundation
 
 /**
 Structure representing a native response to the HTML5
-
-@author Carlos Lozano Diez
-@since v2.0
-@version 1.0
 */
 public class APIBean : NSObject {
     
+    /// Field Description
+    private var field:String? = nil
+    
+    /// Field Description
+    public var field2:String? = nil
+    
     
     /**
-    Default constructor
-    
-    @since v2.0
+    Default Constructor
     */
     public override init() {
         super.init()
     }
     
+    /**
+    Method description
+    
+    :param: param1 Parameter 1 description
+    :param: param2 Parameter 2 description
+    
+    :returns: Returns the sum of two values
+    */
+    public func method(param1:String, param2:String) -> String{
+        
+        return param1 + param2
+    }
     
     /**
-    JSON Serialization and deserialization support.
+    Method description
+    
+    :param: param1 Parameter 1 description
+    :param: param2 Parameter 2 description
+    
+    :returns: Returns the sum of two values
     */
-    struct Serializer {
+    private func method2(param1:String, param2:String) -> String{
+        
+        return param1 + param2
+    }
+    
+    ///-------------------------
+    /// @name Converting Objects
+    ///-------------------------
+    
+    /**
+    *  Struct description
+    */
+    public struct Serializer {
+        
+        /**
+        Internal method description
+        
+        :param: json Json field
+        
+        :returns: Returns Json parameter
+        */
         static func fromJSON(json : String) -> APIBean {
             var data:NSData = json.dataUsingEncoding(NSUTF8StringEncoding)!
             var jsonError: NSError?
